88c46d2cf7bcb3eaf9201bf42adcaba61f022c3e
Khoa Hoang <whoskhoahoang@gmail.com>
2024-08-24 17:45:14 +0900

    Implement navbar click behavior

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,36 +8,40 @@ const Hero = () => (
   <Background color="white">
     <Navbar logo={<Logo xl />}>
       <li>
-        <Link href="/">Instructor</Link>
+        <Link href="/" onClick={(e) => { e.preventDefault(); window.location.replace("/#instructor") }}>Instructor</Link>
       </li>
       <li>
-        <Link href="/">Price</Link>
+        <Link href="/" onClick={(e) => { e.preventDefault(); window.location.replace("/#price") }}>Price</Link>
       </li>
       <li>
-        <Link href="/">Gallery</Link>
+        <Link href="/" onClick={(e) => { e.preventDefault(); window.location.replace("/#gallery") }}>Gallery</Link>
       </li>
       <li>
-        <Link href="/">Access</Link>
+        <Link href="/" onClick={(e) => { e.preventDefault(); window.location.replace("/#access") }}>Access</Link>
       </li>
     </Navbar>
 
     {/* FOCUS HERE */}
-    <Section yPadding="pt-20 pb-32">
+    <Section id='top' yPadding="pt-20 pb-32">
+      TOP
       <Logo></Logo>
     </Section>
-    <Section yPadding="pt-20 pb-32">
+    <Section id='instructor' yPadding="pt-20 pb-32">
+      INSTRUCTOR
       <Logo></Logo>
     </Section>
-    <Section yPadding="pt-20 pb-32">
+    <Section id='price' yPadding="pt-20 pb-32">
+      PRICE
       <Logo></Logo>
     </Section>
-    <Section yPadding="pt-20 pb-32">
+    <Section id='gallery' yPadding="pt-20 pb-32">
+      GALLERY
       <Logo></Logo>
     </Section>
-    <Section yPadding="pt-20 pb-32">
+    <Section id='access' yPadding="pt-20 pb-64">
+      ACCESS
       <Logo></Logo>
     </Section>
-
   </Background>
 );
 
